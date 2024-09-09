7966c8b4b82d613f8208371a05b13d575746fbb4
pallavag <agpallav@gmail.com>
2024-09-08 22:45:59 -0400

    Initial Commit

---
+++ mta-widget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mta-widget
+//
+//  Created by Pallav Agarwal on 9/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mta-widget/mta_widgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  mta_widgetApp.swift
+//  mta-widget
+//
+//  Created by Pallav Agarwal on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct mta_widgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
